3cabfe5e0b81528c61cc2c757e18d25c08389065
André Quintiliano Filho <drequintiliano@gmail.com>
2025-02-13 19:51:32 -0300

    Update topKFrequent.js

--- topKFrequent.js
+++ topKFrequent.js
@@ -3,12 +3,16 @@ var topKFrequent = function(nums, k) {
    
     for (let i =0; i < nums.length; i++) {          
         map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+            /*
+            aqui o map.set(nums[i] -> aqui eu to setando a chave do meu objeto no map, como as chaves vão ser os proprios valores do array
+            (map.get(nums[i]) || 0) + 1 aqui eu seto o valor que eu peguei do map, se for undefined atribuo 0 e incremento +1
+            */
     }
 
      return Array.from(map.entries())
-        .sort((a, b) => b[1] - a[1]) 
-        .slice(0, k)                 
-        .map(entry => entry[0]); 
+        .sort((a, b) => b[1] - a[1]) //aqui ele ta ordenando de forma descrescente
+        .slice(0, k)                 //aqui ele pega os 'k' primeiros, como meu k=2, ele vai pegar os 2 primeiros que mais aparecem
+        .map(entry => entry[0]);     //aqui ele retorna só as chaves do map, ou no caso os numeros que mais apareceram
 };
 
 /*
