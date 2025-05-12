d0ff97f96fa2142e983db348b5202564abfcb2eb
André Quintiliano Filho <drequintiliano@gmail.com>
2025-05-12 15:39:31 -0300

    Update mergeStringsAlternately.js
    explanation about big o

--- mergeStringsAlternately.js
+++ mergeStringsAlternately.js
@@ -4,16 +4,19 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let newArray = '';
+    let newString = '';
     const maxLengthArray = word1.length > word2.length ? word1.length : word2.length 
 
     for(let i = 0; i < maxLengthArray; i++) {
-        newArray = newArray + (word1[i] ?? '') + (word2[i] ?? '');
+        newString = newString + (word1[i] ?? '') + (word2[i] ?? '');
     }
 
-    return newArray;
+    return newString;
 };
 
+//time and space
+//o space é O(1) - porque eu estou guardando uma variavel.
+//o time é O(n) - porque eu vou percorrer a maior string de tamanho (n).
 
 /*
 Input
