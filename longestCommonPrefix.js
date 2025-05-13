32be23e32093a924735a383f2aff5a05f0b75e64
André Quintiliano Filho <drequintiliano@gmail.com>
2025-05-13 14:14:44 -0300

    Create longestCommonPrefix.js

---
+++ longestCommonPrefix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if(strs.length === 0) return '';
+
+    let prefix = strs[0];
+
+    for(let i = 1; i < strs.length; i ++){
+         while(strs[i].indexOf(prefix) != 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+         }
+    }
+
+    return prefix;
+};
+
+/*
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+*/
